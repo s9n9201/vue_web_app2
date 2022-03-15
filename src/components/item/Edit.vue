b40883e3dbf94b401581ce70dfb9bd861ddcbdb1
kent <s9n9201@yahoo.com.tw>
2022-03-15 09:06:16 +0800

    add Submit Feature

--- src/components/item/Edit.vue
+++ src/components/item/Edit.vue
@@ -2,38 +2,44 @@
     <section id="basic-horizontal-layouts">
         <div class="row match-height justify-content-center">
             <div class="col-md-6 col-12">
-                <form class="form form-horizontal">
+                <form class="form form-horizontal" @submit="onSubmit">
                     <div class="form-body">
                         <div class="row">
                             <div class="col-md-4">
                                 <label>商品名稱 {{ IRecId }}</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Name" id="I_Name" class="form-control" value="">
+                                <input type="text" name="I_Name" id="I_Name" class="form-control" v-model="Item.iname">
                             </div>
                             <div class="col-md-4">
                                 <label>貨源</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Source" id="I_Source" class="form-control" value="">
+                                <input type="text" name="I_Source" id="I_Source" class="form-control" v-model="Item.isource">
+                            </div>
+                            <div class="col-md-4">
+                                <label>製造地</label>
+                            </div>
+                            <div class="col-md-8 form-group">
+                                <input type="text" name="I_Source" id="I_MadeIn" class="form-control" v-model="Item.imadeIn">
                             </div>
                             <div class="col-md-4">
                                 <label>數量</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Amount" id="I_Amount" class="form-control" value="">
+                                <input type="text" name="I_Amount" id="I_Amount" class="form-control" v-model="Item.iamount">
                             </div>
                             <div class="col-md-4">
                                 <label>成本</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Cost" id="I_Cost" class="form-control" value="">
+                                <input type="text" name="I_Cost" id="I_Cost" class="form-control" v-model="Item.icost">
                             </div>
                             <div class="col-md-4">
                                 <label>定價</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Price" id="I_Price" class="form-control" value="">
+                                <input type="text" name="I_Price" id="I_Price" class="form-control" v-model="Item.iprice">
                             </div>
                             <div class="col-md-4">
                                 <label>總成本</label>
@@ -97,7 +103,13 @@ export default {
                 },
                 body: JSON.stringify(item),
             });
-        }
+        },
+        onSubmit(e) {
+            e.preventDefault();
+            this.Item.itrecId=2;
+            this.addItem(this.Item);
+            console.log("TEST");
+        },
     },
 }
 </script>
