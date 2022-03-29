30c3fcd00ce132ac3e7fcd6418d01f9a2cc1c084
kent <s9n9201@yahoo.com.tw>
2022-03-29 21:51:48 +0800

    add login router

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,55 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ItemList from "../views/ItemList"
-//import ItemTypeList from "@/views/ItemTypeList";
-import MainTest from "@/views/MainTest"
-//import App from "@/App";
 
 const routes=[
     {
-        path: "/",
-        component: MainTest,
-        name: "MainTest"
+        path: "/login",
+        component: ()=>import("@/views/Login"),
+        name: "Login"
     },
     {
-        path: "/itemlist",
-        name: "ItemList",
-        component: ItemList,
-        meta: {
-            title: "商品管理",
-        },
+        path: "/",
+        component: ()=>import("@/views/Main"),
+        name: "Main",
         children: [
             {
-                path: "",
-                name: "itemOfList",
-                component: ()=>import("../components/item/List"),
+                path: "/itemlist",
+                name: "ItemList",
+                component: ItemList,
+                meta: {
+                    title: "商品管理",
+                },
+                children: [
+                    {
+                        path: "",
+                        name: "itemOfList",
+                        component: ()=>import("../components/item/List"),
+                    },
+                    {
+                        path: "edit",
+                        name: "itemOfEdit",
+                        component: ()=>import("../components/item/Edit"),
+                    },
+                    {
+                        path: "edit/:id",
+                        name: "itemOfEditById",
+                        component: ()=>import("../components/item/Edit"),
+                        props: route=>({
+                            IRecId: route.params.id,
+                        }),
+                    },
+                ]
             },
             {
-                path: "edit",
-                name: "itemOfEdit",
-                component: ()=>import("../components/item/Edit"),
-            },
-            {
-                path: "edit/:id",
-                name: "itemOfEditById",
-                component: ()=>import("../components/item/Edit"),
-                props: route=>({
-                    IRecId: route.params.id,
-                }),
-            },
+                path: "/itemtypelist",
+                name: "ItemTypeList",
+                component: ()=>import("@/views/ItemTypeList"),
+                meta: {
+                    title: "商品類別管理",
+                },
+            }
         ]
     },
-    {
-        path: "/itemtypelist",
-        name: "ItemTypeList",
-        component: ()=>import("@/views/ItemTypeList"),
-        meta: {
-            title: "商品類別管理",
-        },
-    }
 ];
 
 const router=createRouter({
@@ -63,7 +67,9 @@ router.beforeEach((to)=>{
     if (to.meta.title) {
         metaTitle=to.meta.title;
     }
-    window.document.getElementById("headTitle").innerText=metaTitle;
+    if (window.document.getElementById("headTitle")) {
+        window.document.getElementById("headTitle").innerText=metaTitle;
+    }
 });
 
 export default router
