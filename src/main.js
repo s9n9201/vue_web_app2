ccd79e816833a0ee82dc0377ee77405a7f032573
kent <s9n9201@yahoo.com.tw>
2022-03-30 14:09:39 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import "@/assets/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
@@ -10,4 +11,5 @@ import "bootstrap/dist/js/bootstrap.bundle"
 
 createApp(App)
     .use(router)
+    .ues(store)
     .mount('#app')
