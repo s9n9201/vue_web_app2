3e1d4e5ac212b0a477d3e2effe4d9ba3fe5c1891
kent <s9n9201@yahoo.com.tw>
2022-03-31 22:41:42 +0800

    fix key name

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,9 +1,9 @@
 export default function authHeader() {
     let user=JSON.parse(localStorage.getItem("user"));
     let Auth={};
-    if (user && user.token) {
+    if (user && user.Token) {
         Auth={
-            Authorization: "Bearer "+user.token
+            Authorization: "Bearer "+user.Token
         };
     }
     return Auth;
