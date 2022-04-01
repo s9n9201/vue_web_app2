f01926e028bcb0e422715d7042121c6313e80bc5
kent <s9n9201@yahoo.com.tw>
2022-04-01 18:03:33 +0800

    remove div block

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,47 +1,40 @@
 <template>
     <div id="auth">
         <div class="row h-100">
-<!--            <div class="col-lg-12 col-12">-->
-                <div id="auth-left">
-                    <div class="auth-logo">
-                        <a href="index.html"><img src="@/assets/images/logo/logo.png" alt="Logo"></a>
-                    </div>
-                    <h1 class="auth-title">Log in.</h1>
-                    <Form @submit="handleLogin" :validation-schema="schema">
-                        <div class="form-group position-relative has-icon-left ">
-                            <Field type="text" class="form-control form-control-xl" name="orgName" placeholder="帳號"/>
-                            <div class="form-control-icon">
-                                <i class="bi bi-person"></i>
-                            </div>
-                        </div>
-                        <ErrorMessage name="orgName" class="error-feedback"/>
-                        <div class="mb-4"></div>
-                        <div class="form-group position-relative has-icon-left ">
-                            <Field type="password" class="form-control form-control-xl" name="password" placeholder="密碼" autocomplete="off"/>
-                            <div class="form-control-icon">
-                                <i class="bi bi-shield-lock"></i>
-                            </div>
-                        </div>
-                        <ErrorMessage name="password" class="error-feedback"/>
-                        <div class="mb-4"></div>
-                        <div class="form-check form-check-lg d-flex align-items-end">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                                Keep me logged in
-                            </label>
-                        </div>
-                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-                        <div v-if="message" class="alert alert-danger mt-4">
-                            {{ message }}
-                        </div>
-                    </Form>
-                    <div class="text-center mt-5 text-lg fs-4">
-                        <p class="text-gray-600">Don't have an account? <router-link to="register" class="font-bold">Sign
-                            up</router-link>.</p>
-                        <p><a class="font-bold" href="">Forgot password?</a>.</p>
-                    </div>
+            <div id="auth-left">
+                <div class="auth-logo">
+                    <a href="index.html"><img src="@/assets/images/logo/logo.png" alt="Logo"></a>
                 </div>
-<!--            </div>-->
+                <h1 class="auth-title">Log in.</h1>
+                <Form @submit="handleLogin" :validation-schema="schema">
+                    <div class="form-group position-relative has-icon-left ">
+                        <Field type="text" class="form-control form-control-xl" name="orgName" placeholder="帳號"/>
+                        <div class="form-control-icon">
+                            <i class="bi bi-person"></i>
+                        </div>
+                    </div>
+                    <ErrorMessage name="orgName" class="error-feedback"/>
+                    <div class="mb-4"></div>
+                    <div class="form-group position-relative has-icon-left ">
+                        <Field type="password" class="form-control form-control-xl" name="password" placeholder="密碼" autocomplete="off"/>
+                        <div class="form-control-icon">
+                            <i class="bi bi-shield-lock"></i>
+                        </div>
+                    </div>
+                    <ErrorMessage name="password" class="error-feedback"/>
+                    <div class="mb-4"></div>
+                    <div class="form-check form-check-lg d-flex align-items-end">
+                        <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                            Keep me logged in
+                        </label>
+                    </div>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    <div v-if="message" class="alert alert-danger mt-4">
+                        {{ message }}
+                    </div>
+                </Form>
+            </div>
         </div>
     </div>
 </template>
