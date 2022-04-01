c2f3937a157283f54d9fe03a3f4e13aeef81508c
kent <s9n9201@yahoo.com.tw>
2022-04-01 18:03:45 +0800

    add logout method

--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -1,22 +1,44 @@
 <template>
     <div id="sidebar" class="active">
         <div class="sidebar-wrapper active">
-            <div class="sidebar-header">
-                <div class="d-flex justify-content-between">
-                    <div class="logo">
-                        <a href="/"><img src="../assets/images/logo/logo.png" alt="Logo" srcset=""></a>
-                    </div>
-                    <div class="toggler">
-                        <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+            <div class="sidebar-content">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="/"><img src="../assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
                     </div>
                 </div>
+                <div class="sidebar-menu logout-block">
+                    <ul class="menu">
+                        <Menu :MainMenu="MainMenu" :ActiveMenu="ActiveMenu" @add-menu-active="addMenuActive"/>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
             </div>
-            <div class="sidebar-menu">
-                <ul class="menu">
-                    <Menu :MainMenu="MainMenu" :ActiveMenu="ActiveMenu" @add-menu-active="addMenuActive"/>
-                </ul>
+            <div class="logout">
+                <a href="#border-less" data-bs-toggle="modal"><img src="../assets/images/logout.png" alt="logout-icon"></a>
             </div>
-            <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+        </div>
+    </div>
+    <div class="modal fade text-left modal-borderless" id="border-less" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-sm" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">您確定要登出嗎？</h5>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-primary" data-bs-dismiss="modal">
+                        <span>取消</span>
+                    </button>
+                    <button type="button" id="Accept" class="btn btn-primary ml-1" @click="isLogout" data-bs-dismiss="modal">
+                        <span>確定</span>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -38,13 +60,11 @@ export default {
     },
     methods: {
         addMenuActive(MenuIndex) {
-            //console.log("addMenuActive > "+MenuIndex);
             this.ActiveMenu=this.ActiveMenu.map( (menu, index)=> {
                 return MenuIndex===index ? {...menu, isActive: true} : {...menu, isActive: false};
             });
         },
         addSubMenuActive(MenuIndex, SubIndex) {
-            //console.log("addSubMenuActive > "+MenuIndex+" > "+SubIndex+"");
             console.log("TEST");
             this.ActiveMenu=this.ActiveMenu.map( (menuObj, index)=> {
                 menuObj.subArr=menuObj.subArr.map( (subObj, i) => {
@@ -52,6 +72,11 @@ export default {
                 });
                 return {...menuObj, subArr: menuObj.subArr};
             });
+        },
+        isLogout() {
+            this.$store.dispatch("auth/logout").then(
+                ()=>this.$router.push("/login")
+            );
         },
     },
     created() {
@@ -65,19 +90,19 @@ export default {
                         parentid: 1,
                         id: 1,
                         name: "列表 1",
-                        url: "test1",
+                        url: "/test1",
                     },
                     {
                         parentid: 1,
                         id: 2,
                         name: "列表 2",
-                        url: "test2",
+                        url: "/test2",
                     },
                     {
                         parentid: 1,
                         id: 3,
                         name: "列表 3",
-                        url: "test3",
+                        url: "/test3",
                     },
                 ],
             },
@@ -90,7 +115,7 @@ export default {
                         parentid: 2,
                         id: 4,
                         name: "列表 11",
-                        url: "test4",
+                        url: "/test4",
                     },
                     {
                         parentid: 2,
@@ -135,5 +160,18 @@ export default {
 </script>
 
 <style scoped>
+.sidebar-content {
+    min-height: 100%;
+    margin-bottom: -50px;
+}
+
+.logout-block {
+    padding-bottom: 50px;
+}
+
+.logout  {
+    height: 50px;
+    padding-left: 1.6rem;
+}
 
 </style>
