5f95b4ac500fb2dffdcf860c718b21e0e056fe2f
kent <s9n9201@yahoo.com.tw>
2022-04-07 16:49:45 +0800

    fix refreshToken key name

--- src/services/axios-interceptors.js
+++ src/services/axios-interceptors.js
@@ -1,11 +1,10 @@
 import axiosInstance from "@/services/axios-instance";
-import tokenService from "@/services/token.service";
 import TokenService from "@/services/token.service";
 
 const setup=(store)=>{
     axiosInstance.interceptors.request.use(
         (config)=>{
-            const token=tokenService.getLocalAccessToken();
+            const token=TokenService.getLocalAccessToken();
             if (token) {
                 config.headers["Authorization"]="Bearer "+token;
             }
@@ -23,12 +22,11 @@ const setup=(store)=>{
         async (error)=>{
             const originalConfig=error.config;
             if (originalConfig.url!=="/api/signin" && error.response) {
-                console.log(" retry > ", originalConfig._retry);
                 if (error.response.status===401 && !originalConfig._retry) {
                     originalConfig._retry=true;
                     try {
                         const result=await axiosInstance.post("/api/refreshtoken", {
-                            RefreshToken: tokenService.getLocalRefreshToken(),
+                            refreshToken: TokenService.getLocalRefreshToken(),
                         });
                         const { Token }=result.data;
                         store.dispatch("auth/refreshToken", Token);
