4643d34f3b8aba1bd04948248cfd3a4b08cf1850
kent <s9n9201@yahoo.com.tw>
2022-04-10 18:23:17 +0800

    add sweetalert to globalProperties

--- src/main.js
+++ src/main.js
@@ -3,16 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import setupInterceptors from "./services/axios-interceptors"
+import swal from "sweetalert2"
 
 import "@/assets/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "@/assets/css/pages/auth.css"
 import "@/assets/css/app.css"
 import "bootstrap/dist/js/bootstrap.bundle"
-
+import "sweetalert2/dist/sweetalert2.min.css"
 
 setupInterceptors(store);
-createApp(App)
-    .use(router)
+
+swal.mixin({
+    showCloseButton: true,
+})
+
+const app=createApp(App)
+app.config.globalProperties.$Swal=swal
+app.use(router)
     .use(store)
     .mount('#app')
