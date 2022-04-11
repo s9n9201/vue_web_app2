ffd897e49957c026b9fbaf8d4c636f59a87e6baf
kent <s9n9201@yahoo.com.tw>
2022-04-11 19:12:48 +0800

    增加防呆及新增成功跳轉功能。

--- src/components/item/Edit.vue
+++ src/components/item/Edit.vue
@@ -2,44 +2,47 @@
     <section id="basic-horizontal-layouts">
         <div class="row match-height justify-content-center">
             <div class="col-md-6 col-12">
-                <form class="form form-horizontal" @submit="onSubmit">
+                <form class="form form-horizontal" @submit.prevent="onSubmit">
                     <div class="form-body">
                         <div class="row">
                             <div class="col-md-4">
                                 <label>商品名稱 {{ IRecId }}</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Name" id="I_Name" class="form-control" v-model="Item.iname">
+                                <input type="text" name="I_Name" id="I_Name" ref="iname" class="form-control" v-model.trim="Item.iname" @keyup="ItemMsg.iname.val=''">
+                                <div class="error-feedback">{{ ItemMsg.iname.val }}</div>
                             </div>
                             <div class="col-md-4">
                                 <label>貨源</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Source" id="I_Source" class="form-control" v-model="Item.isource">
+                                <input type="text" name="I_Source" id="I_Source" ref="isource" class="form-control" v-model.trim="Item.isource" @keyup="ItemMsg.isource.val=''">
+                                <div class="error-feedback">{{ ItemMsg.isource.val }}</div>
                             </div>
                             <div class="col-md-4">
                                 <label>製造地</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Source" id="I_MadeIn" class="form-control" v-model="Item.imadeIn">
+                                <input type="text" name="I_Source" id="I_MadeIn" ref="imadeIn" class="form-control" v-model.trim="Item.imadeIn" @keyup="ItemMsg.imadeIn.val=''">
+                                <div class="error-feedback">{{ ItemMsg.imadeIn.val }}</div>
                             </div>
                             <div class="col-md-4">
                                 <label>數量</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Amount" id="I_Amount" class="form-control" v-model="Item.iamount">
+                                <input type="text" name="I_Amount" id="I_Amount" class="form-control" v-model.trim="Item.iamount">
                             </div>
                             <div class="col-md-4">
                                 <label>成本</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Cost" id="I_Cost" class="form-control" v-model="Item.icost">
+                                <input type="text" name="I_Cost" id="I_Cost" class="form-control" v-model.trim="Item.icost">
                             </div>
                             <div class="col-md-4">
                                 <label>定價</label>
                             </div>
                             <div class="col-md-8 form-group">
-                                <input type="text" name="I_Price" id="I_Price" class="form-control" v-model="Item.iprice">
+                                <input type="text" name="I_Price" id="I_Price" class="form-control" v-model.trim="Item.iprice">
                             </div>
                             <div class="col-md-4">
                                 <label>總成本</label>
@@ -57,8 +60,13 @@
 <!--                                </div>-->
 <!--                            </div>-->
                             <div class="col-sm-12 d-flex justify-content-end">
-                                <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
-<!--                                <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>-->
+                                <button type="submit"
+                                        class="btn btn-primary me-1 mb-1"
+                                        :class="[buttonData.disabled && `btt-loading`]"
+                                        style="width: 90px;"
+                                        :disabled="buttonData.disabled"
+                                        v-html="buttonData.val">
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -67,8 +75,8 @@
         </div>
     </section>
 </template>
-
 <script>
+import axiosInstance from "@/services/axios-instance";
 
 export default {
     name: "EditComponent",
@@ -85,6 +93,24 @@ export default {
                 iprice: 0,
                 itotal: 0,
             },
+            ItemMsg: {
+                iname: {
+                    val: "",
+                    default: "請輸入商品名稱！",
+                },
+                isource: {
+                    val: "",
+                    default: "請輸入貨源！"
+                },
+                imadeIn: {
+                    val: "",
+                    default: "請輸入製造地！",
+                },
+            },
+            buttonData: {
+                disabled: false,
+                val: "確定送出",
+            },
         }
     },
     props: {
@@ -93,26 +119,60 @@ export default {
         }
     },
     methods: {
-        async addItem(item) {
-            await fetch(this.$store.state.auth.url+"item/insert", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                },
-                body: JSON.stringify(item),
+        addItem(item) {
+            return axiosInstance
+                .post("/item/insert", item)
+                .then(
+                    response=>{
+                        return response.data;
+                    },
+                    error=>{
+                        console.log(error.response);
+                        return error.response;
+                    }
+                );
+        },
+        dataCheck() {
+            return Object.keys(this.ItemMsg).some((key)=>{
+                if (this.Item[key]=="") {
+                    this.ItemMsg[key].val=this.ItemMsg[key].default;
+                    this.$refs[key].focus();
+                    return true;
+                }
             });
         },
-        onSubmit(e) {
-            e.preventDefault();
-            this.Item.itrecId=2;
-            console.log("TEST commit");
-            this.addItem(this.Item);
-            console.log("TEST");
+        buttonDisabled(type) {
+            this.buttonData.disabled=type;
+            this.buttonData.val=type?"<img src='../Spinner.svg' style='width: 32px; heigth: auto;'>":"確認送出";
+        },
+        async onSubmit() {
+            this.buttonDisabled(true);
+            if (this.dataCheck()) {
+                this.buttonDisabled(false);
+                return false;
+            }
+            const result=await this.addItem(this.Item);
+            if (result.status=="200") {
+                this.$Toast.fire({
+                    icon: "success",
+                    title: result.message,
+                });
+                this.buttonDisabled(true);
+                this.$router.push("/itemlist");
+            }
         },
     },
 }
 </script>
 
 <style scoped>
+.error-feedback {
+    color: red;
+    padding: 5px 0px 5px 0px;
+}
 
+.btt-loading {
+    padding-top: 0.1rem;
+    padding-bottom: 0.1rem;
+}
 </style>
