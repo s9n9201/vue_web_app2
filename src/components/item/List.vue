d6f54b4c876e0b3d73520fd1b985641c24155572
kent <s9n9201@yahoo.com.tw>
2022-04-18 16:25:00 +0800

    接住從Itemlist傳入的UrlParameter，讓編輯連結可以有該參數。

--- src/components/item/List.vue
+++ src/components/item/List.vue
@@ -21,7 +21,7 @@
                     <td>{{ Item.iprice }}</td>
                     <td>{{ Item.itotal }}</td>
                     <td>
-                        <router-link :to="'/itemlist/edit/'+Item.irecId" @click="$emit('show-tab', Item.irecId)">編輯</router-link>
+                        <router-link :to="'/itemlist/edit/'+Item.irecId+UrlParameter" @click="$emit('show-tab', Item.irecId)">編輯</router-link>
                     </td>
 <!--                    <td><a href="#"><i class="badge-circle badge-circle-light-secondary font-medium-1" data-feather="mail"></i></a></td>-->
                 </tr>
@@ -36,7 +36,10 @@ export default {
     props: {
         ItemList: {
             type: Array,
-        }
+        },
+        UrlParameter: { //因為這個function return 一個String
+            type: String,
+        },
     },
     mounted() {
         //console.log(this.$route.path);
