b210647abcd0280441920809fc7a764452f661e3
kent <s9n9201@yahoo.com.tw>
2022-05-06 19:21:35 +0800

    增加不同IP的baseURL

--- src/services/axios-instance.js
+++ src/services/axios-instance.js
@@ -1,7 +1,12 @@
 import axios from "axios"
+let serverAddress=["localhost", "114.46.147.253"];
+
+serverAddress=serverAddress.filter((ip)=>{
+    return location.host.indexOf(ip)>-1;
+});
 
 const instance=axios.create({
-    baseURL: "http://localhost:1015",
+    baseURL: "http://"+serverAddress[0]+":1015",
     headers: {
         "Content-Type": "application/json",
     },
